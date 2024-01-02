e8d82c71e678dcd46752ab8a855b0a1ed173aba7
Casual-Beluga <105453111+Casual-Beluga@users.noreply.github.com>
2024-01-02 09:33:10 -0500

    Create Updated_version.c

---
+++ Data_Structure_Using_C/Chapter_4/page119_programming_example/Updated_version.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "console_utils.h"
+
+int main()
+{
+    char str[100], i = 0, length;
+    clrscr();
+    printf("\n Enter the string : ");
+    fgets(str, 100, stdin);
+    while (str[i] != '\0')
+        i++;
+    length = i;
+    printf("\n The length of the string is : %d", length);
+    getch();
+    return 0;
+}
